fc1426314f5c84cd191f683af1c652b0224c7033
Fuad Hasan <68866471+fuadhasan08@users.noreply.github.com>
2023-10-02 22:43:51 +0600

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 import mysql from 'mysql2';
 
 export const db = mysql.createPool({
-  host: 'sql12.freemysqlhosting.net',
-  user: 'sql12644305',
-  password: '59GVdbDMRK',
-  database: 'sql12644305',
+  host: 'jamuna-bd-cp3.hostever.com',
+  user: 'splendi1_bachelors',
+  password: 'AsDfGhJkL123',
+  database: 'splendi1_bachelors',
 });
